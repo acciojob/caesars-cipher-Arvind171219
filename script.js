46db9c211d0551a690935c9eff7cf863d0a17185
Arvind kumar <74968170+Arvind171219@users.noreply.github.com>
2023-06-09 17:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// Your Script here.
-function rot13(encodedString) {
+function rot13() {
+  var encodedString = prompt("Enter a ROT13 encoded string:");
   var decodedString = "";
-  
+
   for (var i = 0; i < encodedString.length; i++) {
     var charCode = encodedString.charCodeAt(i);
-    
+
     if (charCode >= 65 && charCode <= 90) {
       // Decode uppercase letters
       charCode = ((charCode - 65 + 13) % 26) + 65;
     }
-    
+
     decodedString += String.fromCharCode(charCode);
   }
-  
-  return decodedString;
+
+  console.log("Decoded string: " + decodedString);
 }
 
-// Example usage:
-var encoded = "EBG13 rknzcyr.";
-var decoded = rot13(encoded);
-console.log(decoded); 
+rot13();
